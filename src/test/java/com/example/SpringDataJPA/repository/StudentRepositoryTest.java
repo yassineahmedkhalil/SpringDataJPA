2b449075a262a006787a90534eb456a68e3ed685
yassineahmedkhalil <89987265+yassineahmedkhalil@users.noreply.github.com>
2022-02-13 18:59:15 +0100

    StudentRepository and StudentRepositoryTest

--- src/test/java/com/example/SpringDataJPA/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/SpringDataJPA/repository/StudentRepositoryTest.java
@@ -5,9 +5,7 @@ import com.example.SpringDataJPA.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
-
 
 @SpringBootTest
 class StudentRepositoryTest {
@@ -30,15 +28,73 @@ class StudentRepositoryTest {
                 .lastName("yassine")
                 .guardian(guardian)
                 .build();
-
         studentRepository.save(student);
     }
 
     @Test
     public void printAllStudents(){
         List<Student> students = studentRepository.findAll();
-
         System.out.println("Student List" + students);
     }
 
+    @Test
+    public void printStudentByFirstname(){
+        List<Student> students = studentRepository.findByFirstName("Ahmed");
+        System.out.println(students);
+    }
+
+    @Test
+    public void printStudentByCharacter(){
+        List<Student> students = studentRepository.findByFirstNameContaining("as");
+        for(Student s : students){
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void printStudentBasedOnGuardienName(){
+        List<Student> students =studentRepository.findByGuardianName("guardian");
+        for(Student s : students){
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void printStudentByFirstNameAndLastName(){
+        Student student = studentRepository.findByFirstNameAndLastName("ahmed", "yassine");
+        System.out.println(student);
+    }
+
+    @Test
+    public void printgetStundentByEmailaddress(){
+        Student student = studentRepository.getStudentByEmailaddress("ahmed@gmaill.com");
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void printgetStundentFirstNameByEmailaddress(){
+        String stundetnFirstName = studentRepository.getStudentFirstNameByEmailaddress("ahmed@gmaill.com");
+        System.out.println("stundetnFirstName = " + stundetnFirstName);
+    }
+
+    @Test
+    public void printgetStudentByEmailAddressNative(){
+
+        Student student =
+                studentRepository.getStudentByEmailAddressNative("ahmed@gmaill.com");
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void getStudentByEmailAddressNativeNamedParam(){
+        Student student =
+                studentRepository.getStudentByEmailAddressNativeNamedParam("ahmed@gmaill.com");
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void updateStudentNameByEmailId(){
+
+        studentRepository.updateStudentNameByEmailId("khalil","ahmed@gmaill.com");
+    }
 }
